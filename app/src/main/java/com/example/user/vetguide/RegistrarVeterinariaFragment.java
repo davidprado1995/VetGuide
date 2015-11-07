64a82f3843e7d83674cbbebbdec130538b651fb1
Mayumi <cmta2112@gmail.com>
2015-11-07 12:12:25 -0500

    agregando mapa

--- app/src/main/java/com/example/user/vetguide/RegistrarVeterinariaFragment.java
+++ app/src/main/java/com/example/user/vetguide/RegistrarVeterinariaFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,39 +19,46 @@ import com.parse.SaveCallback;
 
 public class RegistrarVeterinariaFragment extends Fragment {
 
-    Button butSiguiente,butCancelar;
-    EditText nombrevete,direccionvete,distritovete,telefonovete,diasinicio,diasfin,horasinicio,horasfin;
+    Button butSiguiente, butCancelar, butMapa;
+    EditText nombrevete, direccionvete, distritovete, telefonovete, diasinicio, diasfin, horasinicio, horasfin;
     ParseObject veterinaria;
+    double latitud, longitud;
+
+    static int UBICACION = 1000;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_registrar_veterinaria, container, false);
+        View view = inflater.inflate(R.layout.fragment_registrar_veterinaria, container, false);
 
-        butSiguiente=(Button)view.findViewById(R.id.buttonSiguiente3);
+        butSiguiente = (Button) view.findViewById(R.id.buttonSiguiente3);
+        butMapa = (Button) view.findViewById(R.id.buttonMapear);
 
-        nombrevete = (EditText)view.findViewById(R.id.eteNombreVet);
-        direccionvete = (EditText)view.findViewById(R.id.eteDireccionVet);
-        distritovete = (EditText)view.findViewById(R.id.eteDistritoVet);
-        telefonovete = (EditText)view.findViewById(R.id.eteTelefonoVet);
-        diasinicio = (EditText)view.findViewById(R.id.eteDiaInicioVet);
-        diasfin = (EditText)view.findViewById(R.id.eteDiaFinVet);
-        horasinicio = (EditText)view.findViewById(R.id.eteHoraInicioVet);
-        horasfin = (EditText)view.findViewById(R.id.eteHoraFinVet);
+        nombrevete = (EditText) view.findViewById(R.id.eteNombreVet);
+        direccionvete = (EditText) view.findViewById(R.id.eteDireccionVet);
+        distritovete = (EditText) view.findViewById(R.id.eteDistritoVet);
+        telefonovete = (EditText) view.findViewById(R.id.eteTelefonoVet);
+        diasinicio = (EditText) view.findViewById(R.id.eteDiaInicioVet);
+        diasfin = (EditText) view.findViewById(R.id.eteDiaFinVet);
+        horasinicio = (EditText) view.findViewById(R.id.eteHoraInicioVet);
+        horasfin = (EditText) view.findViewById(R.id.eteHoraFinVet);
 
         butSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               veterinaria= new ParseObject("Veterinaria");
+                veterinaria = new ParseObject("Veterinaria");
 
 
-                veterinaria.put("nombre",nombrevete.getText().toString());
-                veterinaria.put("direccion",direccionvete.getText().toString());
-                veterinaria.put("distrito",distritovete.getText().toString());
-                veterinaria.put("telefono",Integer.parseInt(telefonovete.getText().toString()));
-                veterinaria.put("dias_atencion",diasinicio.getText().toString() + "-" + diasfin.getText().toString());
+                veterinaria.put("nombre", nombrevete.getText().toString());
+                veterinaria.put("direccion", direccionvete.getText().toString());
+                veterinaria.put("distrito", distritovete.getText().toString());
+                veterinaria.put("telefono", Integer.parseInt(telefonovete.getText().toString()));
+                veterinaria.put("dias_atencion", diasinicio.getText().toString() + "-" + diasfin.getText().toString());
                 veterinaria.put("horas_atencion", horasinicio.getText().toString() + "-" + horasfin.getText().toString());
+                veterinaria.put("latitud",latitud);
+                veterinaria.put("longitud",longitud);
                 veterinaria.saveInBackground(new SaveCallback() {
 
                     @Override
@@ -59,7 +67,7 @@ public class RegistrarVeterinariaFragment extends Fragment {
                             Toast t = Toast.makeText(getContext(), "Inscripción de Veterinaria Correcta", Toast.LENGTH_SHORT);
                             t.show();
                             Intent i = new Intent(getActivity(), MascotasVetActivity.class);
-                            i.putExtra("codigovet",veterinaria.getObjectId());
+                            i.putExtra("codigovet", veterinaria.getObjectId());
                             startActivity(i);
                         } else {
                             Toast t = Toast.makeText(getContext(), "Inscripción de Veterinaria Incorrecta", Toast.LENGTH_SHORT);
@@ -69,23 +77,43 @@ public class RegistrarVeterinariaFragment extends Fragment {
                 });
 
 
-
             }
         });
 
 
-        butCancelar=(Button)view.findViewById(R.id.buttonCancelar3);
+        butCancelar = (Button) view.findViewById(R.id.buttonCancelar3);
         butCancelar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(getActivity(),MainActivity.class);
+                Intent i = new Intent(getActivity(), MainActivity.class);
                 startActivity(i);
             }
         });
 
+        butMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i0 = new Intent(getActivity(), MapsActivity.class);
+
+                startActivityForResult(i0,UBICACION);
+
+            }
+        });
+
+
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == UBICACION){
+            if(resultCode == Activity.RESULT_OK){
+                latitud = data.getDoubleExtra("latitud",0);
+                longitud =data.getDoubleExtra("longitud",0);
 
+            }
+        }
 
+    }
 }
